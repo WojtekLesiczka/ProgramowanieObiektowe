015c9728ec8025eb10ae5c22b0b9a8041fbae2e6
1 <chuj@onet.eu>
2023-10-25 13:21:26 +0200

    innit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using lab2.constructions;
+
+var constr = new Construction(32,23,3,6,"brik");
+Console.WriteLine(constr._buildmaterials);
---
+++ constructions/Construction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2.constructions
+{
+    internal class Construction
+    {
+        public Construction()
+        {
+        }
+
+        public Construction(float height, float width, int entrances, int humancapacity, string buildmaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humancapacity;
+            _buildmaterial = buildmaterial;
+        }
+        private string _buildmaterial { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial
+        {
+            get
+            {
+                return _buildmaterial;
+            }
+            set { _buildmaterial = value; }
+        }
+    }
+}
